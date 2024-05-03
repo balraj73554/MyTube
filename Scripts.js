783a75f8537e1c02b5c153b784cd51da33548bb7
balraj73554 <160026576+balraj73554@users.noreply.github.com>
2024-05-03 17:22:11 +0530

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,8 @@
+
+// Hamburger Js
+function toggleMenu() {
+    var options = document.getElementById("options");
+    options.style.display === "block" ? options.style.display = "none" : options.style.display = "block";
+    var hamburger = document.querySelector(".hamburger");
+    hamburger.classList.toggle("change");
+}
